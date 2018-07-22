1b4708cb1751b2a46c1b5da65eb3633c0e534aec
Audrey Ha <audreyha@gmail.com>
2018-07-21 22:59:12 -0700

    Added more code for saving the song title, composer, event, and date

--- MusicJournal/ViewControllers/MyRecordingsTableViewController.swift
+++ MusicJournal/ViewControllers/MyRecordingsTableViewController.swift
@@ -11,20 +11,67 @@ import UIKit
 
 class MyRecordingsTableViewController: UITableViewController{
     
+    var eventArray = [String]()
+    var recordings = [Recording](){
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        tableView.delegate=self
+        tableView.dataSource=self
+        let headerView=UIView()
+//        headerView.backgroundColor = UIColor(red: 234/255, green: 231/255, blue: 220/255, alpha: 1)
+        headerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 210)
+        
+        tableView.tableHeaderView = headerView
+        
+    }
     @IBAction func unwindToMyRecordings(_ segue: UIStoryboardSegue){
         
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 10
+        return recordings.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell=tableView.dequeueReusableCell(withIdentifier: "myRecordingsTableViewCell", for: indexPath) as! myRecordingsTableViewCell
-        cell.songTitle.text="Song Title"
-        cell.songDate.text="Modification time"
-        cell.songComposer.text="Song's Composer"
-        cell.songEvent.text="Song's Event"
+        let recording=recordings[indexPath.row]
+        cell.songTitle.text=recording.songTitle
+        cell.songDate.text=recording.songDate.convertToString()
+        cell.songComposer.text=recording.songComposer
+        cell.songEvent.text=recording.songEvent
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == .delete{
+            recordings.remove(at: indexPath.row)
+        }
+    }
+        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        guard let identifier=segue.identifier else {return}
+        
+        switch identifier{
+        case "displayMade":
+            guard let indexPath=tableView.indexPathForSelectedRow else{return}
+            
+            let recording=recordings[indexPath.row]
+            let destination=segue.destination as! RecordMusicViewController
+            destination.recording=recording
+        
+        case "new":
+            print("create note bar button item tapped")
+        
+        default:
+            print("unexpected segue identifier")
+            
+        }
     }
 }
--- MusicJournal/ViewControllers/RecordMusicViewController.swift
+++ MusicJournal/ViewControllers/RecordMusicViewController.swift
@@ -10,7 +10,57 @@ import Foundation
 import UIKit
 
 class RecordMusicViewController: UIViewController{
+    var recording: Recording?
+    
     @IBOutlet weak var startNew: UIButton!
+    
+    @IBOutlet weak var songLabel: UITextField!
+    @IBOutlet weak var composerLabel: UITextField!
+    @IBOutlet weak var eventLabel: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        
+        if let recording=recording{
+            songLabel.text=recording.songTitle
+            eventLabel.text=recording.songEvent
+            composerLabel.text=recording.songComposer
+        } else{
+            songLabel.text=""
+            composerLabel.text=""
+            eventLabel.text=""
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        guard let identifier = segue.identifier,
+            let destination=segue.destination as? MyRecordingsTableViewController
+            else {return}
+        
+        switch identifier{
+        case "save" where recording != nil:
+            recording?.songTitle=songLabel.text ?? ""
+            recording?.songEvent=eventLabel.text ?? ""
+            recording?.songComposer=composerLabel.text ?? ""
+            recording?.songDate=Date()
+            
+            destination.tableView.reloadData()
+        
+        case "save" where recording == nil:
+            let recording = Recording()
+            recording.songTitle=songLabel.text ?? ""
+            recording.songEvent=eventLabel.text ?? ""
+            recording.songComposer=composerLabel.text ?? ""
+            recording.songDate=Date()
+            destination.recordings.append(recording)
+            
+        case "cancel":
+            print("cancel bar button item tapped")
+        
+        default:
+            print("unexpected segue!")
+        }
+    }
     
     override func viewDidLoad(){
         super.viewDidLoad()
